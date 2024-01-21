35d6b746ea837bb2ed17eef038c85c836fa318e4
David <avilagarciajesusdavid41@gmail.com>
2024-01-21 15:57:05 -0600

    cambio david

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Daniel");
-=======
-Console.WriteLine("Prueba");
->>>>>>> refs/remotes/origin/main
+Console.WriteLine("David"); 
+
