b115aff90ebe35f54c52cc87e8110fd04733653a
Daniel Escobar <daanielescobar@gmail.com>
2024-01-21 22:29:42 -0600

    add some comments

--- Maquina.cs
+++ Maquina.cs
@@ -5,13 +5,14 @@ namespace Maquina
     public class Maquina
     {
         public void Saludar() {
-            Console.WriteLine("Hello World Desde maquina    !");
+            Console.WriteLine("Hello World Desde maquina!");
             // Pasos para comprar un producto
             // 1.- Se pide al usuario que escriba el codigo de producto que desea comprar
             // 2.- Cada vez que inserte dinero, se muestra su saldo disponible, hasta igualar o superar el precio
             //     del producto seleccionado.
             // 3.- Una vez llego al saldo, se inicia la entrega del producto.
-            // 4/.- Si se necesita cambio, se verifica que haya suficiente.
+            // 4.- Si se necesita cambio, se verifica que haya suficiente.
+            // 5.- Se vuelve al paso 1
         }
     }
 }
