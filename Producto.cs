295ec3fda108d38db6c4870bd4f0110307cff548
David <avilagarciajesusdavid41@gmail.com>
2024-01-21 21:46:50 -0600

    Cambio Produtco

---
+++ Producto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Producto
+{
+    public class Productos
+    {
+        public string name;
+        public double price;
+        public string code;
+        public int stock;
+    
+    }
+}
