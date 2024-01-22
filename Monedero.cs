5231c5da4522af6ab4adc2e0fee1d06a845e1ec5
Daniel Escobar <daanielescobar@gmail.com>
2024-01-21 22:07:53 -0600

    Update var names

--- Monedero.cs
+++ Monedero.cs
@@ -3,16 +3,25 @@ namespace Monedero {
         // El monedero necesita iniciar con cierta cantidad de monedas de cada denominacion.
         // El monedero puede ejecutar una suma del dinero, una resta del cambio.
 
-        int 1_coins;
-        int 2_coins;
-        int 5_coins;
-        int 10_coins;
-        int 20_bill;
-        int 50_bill;
+        int amount_of_1_coins;
+        int amount_of_2_coins;
+        int amount_of_5_coins;
+        int amount_of_10_coins;
+        int amount_of_20_bill;
+        int amount_of_50_bill;
+
+        Monedero(amount_of_1_coins, amount_of_2_coins, amount_of_5_coins, amount_of_10_coins, amount_of_20_bill, amount_of_50_bill){
+            this.amount_of_1_coins = amount_of_1_coins;
+            this.amount_of_2_coins = amount_of_2_coins;
+            this.amount_of_5_coins = amount_of_5_coins;
+            this.amount_of_10_coins = amount_of_10_coins;
+            this.amount_of_20_bill = amount_of_20_bill;
+            this.amount_of_50_bill = amount_of_50_bill;
+        }   
 
         int TotalMoney {
             get {
-                return (1_coins * 1) + (2_coins * 2) + (5_coins * 5) + (10_coins * 10) + (20_bill * 20) + (50_bill * 50);
+                return (amount_of_1_coins * 1) + (amount_of_2_coins * 2) + (amount_of_5_coins * 5) + (amount_of_10_coins * 10) + (amount_of_20_bill * 20) + (amount_of_50_bill * 50);
             }
 
             set {
