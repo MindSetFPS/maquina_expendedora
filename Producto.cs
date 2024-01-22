d072cc7d66c4be9765c7e205c9fe22855f8d31f1
Daniel Escobar <daanielescobar@gmail.com>
2024-01-21 23:51:49 -0600

    Assign vars to object

--- Producto.cs
+++ Producto.cs
@@ -2,12 +2,19 @@ using System;
 
 namespace Producto
 {
-    public class Productos
+    public class Producto
     {
         public string name;
         public double price;
         public string code;
         public int stock;
+
+        public Producto(string name, double price, string code, int stock) {
+            this.name = name;
+            this.price = price;
+            this.code = code;
+            this.stock = stock;
+        }   
     
     }
 }
