8a7dd09e40320ec36b0d1373f10c7e02eee2db60
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2024-01-24 09:59:10 -0600

    Create Dincero class

---
+++ Dinero.cs
@@ -0,0 +1,17 @@
+
+namespace Dinero
+{
+    public class Dinero
+    {
+        public int valor;
+        public int cantidad;
+        public string tipo;
+
+        public Dinero(int valor, int cantidad, string  tipo){
+            this.valor = valor;
+            this.cantidad = cantidad;
+            this.tipo = tipo;
+        }
+
+    }
+}
