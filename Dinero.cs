3eee5b3678cef5e9e00e38c7e70d2a8cf5e4399e
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2024-02-02 06:46:40 -0600

    Add clone method

--- Dinero.cs
+++ Dinero.cs
@@ -3,15 +3,23 @@ namespace Dinero
 {
     public class Dinero
     {
-        public int valor;
-        public int cantidad;
+        public uint valor;
+        public uint cantidad;
         public string tipo;
 
-        public Dinero(int valor, int cantidad, string  tipo){
+        public Dinero(uint valor, uint cantidad, string  tipo){
             this.valor = valor;
             this.cantidad = cantidad;
             this.tipo = tipo;
         }
+
+        public Dinero Clone(){
+            return new Dinero(
+                valor = this.valor,
+                cantidad = this.cantidad,
+                tipo = this.tipo
+            );
+        } 
 
     }
 }
